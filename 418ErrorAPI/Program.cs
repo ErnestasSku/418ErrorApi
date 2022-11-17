2d0600b70e31ed7d81dd1f02d9508a0b4dfb1ca2
ErnestasSku <ernestassku@gmail.com>
2022-11-18 01:51:07 +0200

    Add schema to UI

--- 418ErrorAPI/Program.cs
+++ 418ErrorAPI/Program.cs
@@ -1,3 +1,7 @@
+using _418ErrorAPI.Models;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.DocumentFilter<AddSchemaFilter<Business>>();
+    options.DocumentFilter<AddSchemaFilter<Customer>>();
+    options.DocumentFilter<AddSchemaFilter<Discount>>();
+    options.DocumentFilter<AddSchemaFilter<Employee>>();
+    options.DocumentFilter<AddSchemaFilter<Loyalty>>();
+    options.DocumentFilter<AddSchemaFilter<Order>>();
+    options.DocumentFilter<AddSchemaFilter<OrderItem>>();
+    options.DocumentFilter<AddSchemaFilter<Product>>();
+    options.DocumentFilter<AddSchemaFilter<PromoCode>>();
+    options.DocumentFilter<AddSchemaFilter<Refund>>();
+    options.DocumentFilter<AddSchemaFilter<Report>>();
+    options.DocumentFilter<AddSchemaFilter<Role>>();
+    options.DocumentFilter<AddSchemaFilter<Storage>>();
+    options.DocumentFilter<AddSchemaFilter<StorageItem>>();
+    options.DocumentFilter<AddSchemaFilter<Tax>>();
+    options.DocumentFilter<AddSchemaFilter<TaxProduct>>();
+    options.DocumentFilter<AddSchemaFilter<TimeTable>>();
+    options.DocumentFilter<AddSchemaFilter<UserLoyalty>>();
+
+});
 
 var app = builder.Build();
 
@@ -26,3 +51,31 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//Generic class which allows to use reflection and add schemas to the swagger ui without using them.
+public class AddSchemaFilter<T>: IDocumentFilter
+    where T : class, new()
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var schema = new OpenApiSchema() { };
+        var temp = new T();
+        var properties = typeof(T).GetProperties();
+
+        foreach (var p in properties)
+            schema.Properties.Add(new KeyValuePair<string, OpenApiSchema>(
+                p.Name,
+                new OpenApiSchema()
+                {
+                    Type = p.PropertyType.ToString(),
+                    //Description TODO: maybe add a better descrption.
+                }));
+
+        if (!context.SchemaRepository.Schemas.ContainsKey(typeof(T).Name))
+        {
+            context.SchemaRepository.Schemas.Add(typeof(T).Name, schema);
+        }
+    }
+
+}
